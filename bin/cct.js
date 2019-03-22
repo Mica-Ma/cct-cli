8fa70e6226c74cc42ae0f775df9f7a0928baa461
machao <272110541@qq.com>
2019-03-22 18:19:11 +0800

    Conf

--- bin/cct.js
+++ bin/cct.js
@@ -5,7 +5,13 @@ const chalk = require('chalk')
 const requiredVersion = require('../package.json').engines.node
 const { checkNodeVersion, cleanArgs } = require('../lib/utils/util')
 
-// node version check
+const conf = require('../lib/Conf')
+
+
+// 错误信息
+require('../lib/utils/enhanceErrorMessages')
+
+// 检查node版本
 checkNodeVersion(requiredVersion, 'cct-cli')
 
 program
@@ -19,11 +25,43 @@ program
     require('../lib/commands/init')(name, cleanArgs(cmd))
   })
 
+program
+  .command('config')
+  .description('🛠  global config for cct-cli')
+  .option('-r, --reset', 'Reset global config')
+  .option(
+    '-f, --filters <ruleName>',
+    'Add filter rules for replace files'
+  )
+  .action((cmd) => {
+    // require('../lib/commands/config')(cleanArgs(cmd))
+  })
+
+program
+  .command('ls')
+  .description('📗  list all templates (force fresh cached templates)')
+  .action((cmd) => {
+    // require('../lib/commands/list')(cleanArgs(cmd))
+    console.log(conf.getConfig());
+    
+  })
+
+
 program.on('--help', () => {
   console.log()
   console.log(`  Run ${chalk.cyan(`vue <command> --help`)} for detailed usage of given command.`)
   console.log()
 })
+
+program
+  .arguments('<command>')
+  .action((cmd) => {
+    program.outputHelp()
+    console.log(`  ` + chalk.red(`Unknown command ${chalk.yellow(cmd)}.`))
+    console.log()
+  })
+
+program.commands.forEach((c) => c.on('--help', () => console.log()))
 
 program.parse(process.argv)
 
